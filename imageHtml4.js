a6ab24a3524d37bf0aa235a993de312d7b8301c6
Chad <cdalten@gmail.com>
2026-02-18 01:21:22 -0800

    Create imageHtml4.js

---
+++ imageHtml4.js
@@ -0,0 +1,20 @@
+//Written by Chad Altenburg
+//Copyright 2025
+
+
+//The html is auto generated 
+//by the Android client app.
+//This is because Neocities is static.
+
+//I need this to make global changes
+//to all the prebuilt html files instead
+//of rebuilding each file to reflect
+//the changes.
+function message() {
+  window.onload = function() {
+    document.getElementById("message").innerHTML="Please wait for the comments to fully load." ;
+  }
+}
+
+ 
+message();
